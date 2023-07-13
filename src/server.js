221b61177300f62274d87b45f5e3dc2ce91cf3c3
Majomon <mauri.monzon91@gmail.com>
2023-07-13 11:13:36 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+// Para que el server pueda usar routes
+const express = require("express");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+
+// Creando el server con Express
+const server = express();
+
+//  Transforma en objeto JSON
+server.use(express.json());
+
+// Nos da información sobre lo que pasa en el servidor
+server.use(morgan("dev"));
+
+// Permisos - Controla el acceso al servidor
+server.use(cors());
+
+// El server esta habilitado para usar router
+server.use("/", router);
+
+
+module.exports = server;
